c666eb5dfdcc68cf1c565860acc381874e564423
gbaka01 <gore.goli@gmail.com>
2022-04-26 19:18:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site greta</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"/>
+ </head>
+<body >
+
+        <img class="figure" src="logo_greta.jpg" alt="greta"/> 
+
+   <div class="bienvenue"><img class="figure2" src="triangles-couleurs.png" alt="greta1"/>
+   <p id="site">Bienvenue sur le site du GRETA </p>
+   <p id="site_1"> Une plateforme de discussion entre les stagiaires de la formation et les professionnels</p> 
+   </div>
+
+   
+   <div class="stagiaire"> 
+    <div class="plus-d-info"> 
+        <div class="site_3"> 
+          <div class="groupe1">
+            <div class="rectangle"><button class="site_4"> En savoir plus </button></div>
+          </div>
+        </div>
+        
+            <p id="site_5">Voici toutes les informations qui pourrait vous être utiles.</p>
+            </div> 
+            <div class="titre">
+            <p id="site_6">Vous êtes un stagiaire ?</p> 
+            <div class="rectangle2"></div>
+            </div>
+            <div class="employeur">
+            <div class="employeur1"><div class="site_7"><button class="groupe2"><p id="site_8">En savoir plus</p><div class="rectangle3"></div></button></div>
+            <p id="site_9">Apprenez en plus
+sur les différents
+corps de métiers de
+formation</p>
+</div>
+<p id="site_10">Vous êtes un
+employeur?</p>
+<div class="rectangle4">
+
+</div>
+</div>
+<div class="stats">
+    <div class="stats1">
+        <p id="site_11">En Savoir plus</p><div class="rectangle5"></div><p id="site_12">Découvrez en plus sur les débouchés de nos
+formations</p><p id="site_13">Besoin d’information
+supplémentaires?</p><div class="rectangle6">
+</div>
+</div>
+</div>
+<div class="foot">
+    <div class="copy">
+        <div class="line6"></div><p id="site_14">Greta de l’essonne @ 2018</p>
+        </div>
+        <div class="nav-bar">
+            <div class="profil"><p id="site_16">Profil du stagiaire</p></div>
+            <div class="recherche"><p id="site_17">Recherche stage professionnel</p><div class="line5"></div></div>
+        </div>
+        <div class="statistiques">
+            <div class="line4"></div><p id="site_18">Statistiques de la formation</p><div class="line3"></div>
+    </div>
+    <div class="compte">
+        <p id="site_19">Compte</p>
+    </div>
+<div class="contact">
+    <div class="mail-tel">
+        <div class="mail"><p id="site_20">greta.essonne@ac-versailles.fr</p>
+    </div>
+    <div class="tel"><p id="site_21">01.64.90.91.91</p>
+</div>
+</div>
+</div>
+<div class="reseaux_sociaux">
+    <p id="site_22">Réseaux sociaux</p><div class="line2"></div><div class="line1"></div><p id="site_23">Nous contacter</p>
+</div>
+<div class="rectangle7">
+
+</div>
+
+
+</div>
+</body>
+</html>
