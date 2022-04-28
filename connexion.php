811b7c365d375ec7ba3df91d2350f9b9136d01c2
Gbaka01 <78058739+Gbaka01@users.noreply.github.com>
2022-04-28 18:08:11 +0200

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,55 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+<link rel="stylesheet" href="style1.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+    <img class="figure" src="logo_greta.jpg" alt="greta">
+    <div class="connexion1"><div class="line1"></div><div class="line3"></div><div class="input"><div class="default"><div class="rectangle1"></div><p id="email">Email</p></div><div class="select"><p id="mot">Mot de passe</p><div class="frame6"><div class="arrow"></div><div class="rectangle2"></div>
+</div>
+</div>
+</div>
+    <p id="connexion2">Connexion</p>
+    <div class="connexion">
+        <div class="se_connecter">
+            <p id="se_connecter1">Se connecter</p><div class="rectangle3"></div>
+</div>
+<div class="linkedin">
+    <p id="se_connecter2">Se connecter avec Linkedin</p>
+    <div class="rectangle4"></div>
+</div>
+<div class="créer">
+    <p class="se_connecter3">Créer un compte
+
+</p><div class="rectangle5"></div>
+</div>
+<p id="mot2">Mot de passe oublié</p>
+<div class="rectangle6"></div>
+<footer>
+    <div class="linear_bot"></div><div class="linear_top"></div>
+    <div class="nav-bar"><div class="compte"><div class="line6"></div><p id="compte1">Compte</p>
+</div>
+<div class="statistiques"><div class="line7"></div><p class="statistiques1">Statistiques de la formation</p>
+</div>
+<div class="Profil"><div class="line8"></div><p id="Profil1">Profil du stagiaire</p>
+</div>
+<div class="recherche"><div class="line10"></div><p id="recherche1">Recherche stage professionnel</p></div>
+</div>
+    <div class="contact"><div class="mail-tel"></div><div class="reseaux_sociaux"><p id="reseaux_sociaux1">Réseaux sociaux</p>
+</div>
+<div class="line4"></div>
+<p id="nous_contacter">Nous contacter</p>
+</div>
+    <div class="copy"><p id="greta1">Greta de l"essonne @ 2018</p></div></footer>
+</div>
+    </div>
+</body>
+</html>
