ffad765def0ed9dae5a5c8097eba347c5319278c
Gbaka01 <78058739+Gbaka01@users.noreply.github.com>
2022-05-03 16:21:53 +0200

    Add files via upload

---
+++ stage.php
@@ -0,0 +1,50 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stage</title>
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+      <img class="figure" src="logo_greta.jpg" alt="greta">
+      <div class="stage"> <p id="stage1">stage</p> <p id="type">Type de formation</p><div class="frame10"><div class="rectangle"></div>
+    </div>
+<div class="frame11">
+    <div class="profil">
+
+<div class="en-tête"> <p id="google">google</p> <p id="stage2">Stage:Developpeur web/mobile</p>
+
+</div>
+<div class="description"><p id="description1">Description</p></div>
+
+<div class="informations"> <p id="adresse">Adresse: 20, rue du pavé, Paris</p> <p id="durée">Durée:6 mois</p>
+</div>
+</div>
+</div>
+<footer>
+    <div class="linear_bot"></div><div class="linear_top"></div>
+    <div class="nav-bar"><div class="compte"><div class="line6"></div><p id="compte1">Compte</p>
+</div>
+<div class="statistiques"><div class="line7"></div><p class="statistiques1">Statistiques de la formation</p>
+</div>
+<div class="Profil"><div class="line8"></div><p id="Profil1">Profil du stagiaire</p>
+</div>
+<div class="recherche"><div class="line10"></div><p id="recherche1">Recherche stage professionnel</p></div>
+</div>
+    <div class="contact"><div class="mail-tel"></div><div class="reseaux_sociaux"><p id="reseaux_sociaux1">Réseaux sociaux</p><div class="rectangle3"></div>
+</div>
+<div class="line4"></div>
+<p id="nous_contacter">Nous contacter</p>
+</div>
+    <div class="copy"><p id="greta1">Greta de l"essonne @ 2018</p></div>
+    
+</footer>
+    </div>
+</body>
+</html>
