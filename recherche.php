b3a0c7dc6d1c40416722eeebe8f260b89a566428
Gbaka01 <78058739+Gbaka01@users.noreply.github.com>
2022-05-06 21:29:50 +0200

    Add files via upload

---
+++ recherche.php
@@ -0,0 +1,66 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recherche</title>
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <img class="figure" src="logo_greta.jpg" alt="greta">
+        <div class="stage"> <p id="stage1">Recherche de stage</p> <p id="type">Type de formation</p><div class="frame10"><div class="rectangle"></div>
+    </div>
+    </div>
+    <div class="Profils"><div class="Profil1"><div class="frame1"><div class="projet"><input class="styled1"
+       type="button"
+       value="projet"></div><div class="CV"><input class="styled2"
+       type="button"
+       value="CV"></div></div><div class="photo"></div><p id="nom1">Jackson</p><p id="prenom1">Arlene</p><p id="age1">29 ans</p><p id="metier1">President of Sales</p><p id="tel1">(+33)6 55 58 55 63</p><p id="mail1">willie.jennings@example.com</p></div>
+       <div class="Profil2"><div class="frame2"><div class="projet1"><input class="styled1"
+       type="button"
+       value="projet"></div><div class="CV1"><input class="styled2"
+       type="button"
+       value="CV"></div></div><div class="photo1"></div><p id="nom2">Jackson</p><p id="prenom3">Arlene</p><p id="age2">29 ans</p><p id="metier2">President of Sales</p><p id="tel1">(+33)6 55 58 55 63</p><p id="mail2">willie.jennings@example.com</p></div>
+       <div class="Profil3"><div class="frame3"><div class="projet2"><input class="styled1"
+       type="button"
+       value="projet"></div><div class="CV2"><input class="styled2"
+       type="button"
+       value="CV"></div></div><div class="photo2"></div><p id="nom3">Jackson</p><p id="prenom3">Arlene</p><p id="age3">29 ans</p><p id="metier3">President of Sales</p><p id="tel1">(+33)6 55 58 55 63</p><p id="mail3">willie.jennings@example.com</p></div>
+       <div class="Profil4"><div class="frame4"><div class="projet"><input class="styled1"
+       type="button"
+       value="projet"></div><div class="CV3"><input class="styled2"
+       type="button"
+       value="CV"></div></div><div class="photo3"></div><p id="nom4">Jackson</p><p id="prenom4">Arlene</p><p id="age4">29 ans</p><p id="metier4">President of Sales</p><p id="tel1">(+33)6 55 58 55 63</p><p id="mail4">willie.jennings@example.com</p></div>
+       <div class="Profil5"><div class="frame5"><div class="projet"><input class="styled1"
+       type="button"
+       value="projet"></div><div class="CV4"><input class="styled2"
+       type="button"
+       value="CV"></div></div><div class="photo4"></div><p id="nom5">Jackson</p><p id="prenom5">Arlene</p><p id="age5">29 ans</p><p id="metier5">President of Sales</p><p id="tel5">(+33)6 55 58 55 63</p><p id="mail5">willie.jennings@example.com</p></div>
+    
+    <footer>
+    <div class="linear_bot"></div><div class="linear_top"></div>
+    <div class="nav-bar"><div class="compte"><div class="line6"></div><p id="compte1">Compte</p>
+</div>
+<div class="statistiques"><div class="line7"></div><p class="statistiques1">Statistiques de la formation</p>
+</div>
+<div class="Profil"><div class="line8"></div><p id="Profil1">Profil du stagiaire</p>
+</div>
+<div class="recherche"><div class="line10"></div><p id="recherche1">Recherche stage professionnel</p></div>
+</div>
+
+    <div class="contact"><div class="mail-tel"></div><div class="reseaux_sociaux"><p id="reseaux_sociaux1">Réseaux sociaux</p><div class="rectangle3"></div>
+</div>
+<div class="line4"></div>
+<p id="nous_contacter">Nous contacter</p>
+</div>
+    <div class="copy"><p id="greta1">Greta de l'essonne @ 2018</p></div>
+    
+</footer>
+    </div>
+</body>
+</html>
