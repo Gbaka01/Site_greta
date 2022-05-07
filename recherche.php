7e23a98080421f51233710a848b1a0133203d91a
Gbaka01 <78058739+Gbaka01@users.noreply.github.com>
2022-05-07 17:12:07 +0200

    Add files via upload

--- recherche.php
+++ recherche.php
@@ -25,22 +25,22 @@
        type="button"
        value="projet"></div><div class="CV1"><input class="styled2"
        type="button"
-       value="CV"></div></div><div class="photo1"></div><p id="nom2">Jackson</p><p id="prenom3">Arlene</p><p id="age2">29 ans</p><p id="metier2">President of Sales</p><p id="tel1">(+33)6 55 58 55 63</p><p id="mail2">willie.jennings@example.com</p></div>
+       value="CV"></div></div><div class="photo1"></div><p id="nom2">Warren</p><p id="prenom3">Cameron</p><p id="age2">25 ans</p><p id="metier2">Nursing Assistant</p><p id="tel1">(+33)6 55 59 32 88</p><p id="mail2">kenzi.lawson@example.com</p></div>
        <div class="Profil3"><div class="frame3"><div class="projet2"><input class="styled1"
        type="button"
        value="projet"></div><div class="CV2"><input class="styled2"
        type="button"
-       value="CV"></div></div><div class="photo2"></div><p id="nom3">Jackson</p><p id="prenom3">Arlene</p><p id="age3">29 ans</p><p id="metier3">President of Sales</p><p id="tel1">(+33)6 55 58 55 63</p><p id="mail3">willie.jennings@example.com</p></div>
+       value="CV"></div></div><div class="photo2"></div><p id="nom3">Martinez</p><p id="prenom3">Cody</p><p id="age3">35 ans</p><p id="metier3">Dog Trainer</p><p id="tel1">(+33)6 55 58 51 05 09</p><p id="mail3">tanya.hill@example.com</p></div>
        <div class="Profil4"><div class="frame4"><div class="projet"><input class="styled1"
        type="button"
        value="projet"></div><div class="CV3"><input class="styled2"
        type="button"
-       value="CV"></div></div><div class="photo3"></div><p id="nom4">Jackson</p><p id="prenom4">Arlene</p><p id="age4">29 ans</p><p id="metier4">President of Sales</p><p id="tel1">(+33)6 55 58 55 63</p><p id="mail4">willie.jennings@example.com</p></div>
+       value="CV"></div></div><div class="photo3"></div><p id="nom4">Anderson</p><p id="prenom4">Kyle</p><p id="age4">22 ans</p><p id="metier4">Nursing Assistant</p><p id="tel1">(+33)7 35 55 84 97</p><p id="mail4">georgia.young@example.com</p></div>
        <div class="Profil5"><div class="frame5"><div class="projet"><input class="styled1"
        type="button"
        value="projet"></div><div class="CV4"><input class="styled2"
        type="button"
-       value="CV"></div></div><div class="photo4"></div><p id="nom5">Jackson</p><p id="prenom5">Arlene</p><p id="age5">29 ans</p><p id="metier5">President of Sales</p><p id="tel5">(+33)6 55 58 55 63</p><p id="mail5">willie.jennings@example.com</p></div>
+       value="CV"></div></div><div class="photo4"></div><p id="nom5">Garcia</p><p id="prenom5">Angel</p><p id="age5">29 ans</p><p id="metier5">Marketing Coordinator</p><p id="tel5">(+33)7 45 55 87 71</p><p id="mail5">michael.mitc@example.com</p></div>
     
     <footer>
     <div class="linear_bot"></div><div class="linear_top"></div>
