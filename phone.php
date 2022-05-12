d53b890cc6db754750a4ab19cdedde347d1a472e
Gbaka01 <78058739+Gbaka01@users.noreply.github.com>
2022-05-12 16:05:03 +0200

    Add files via upload

---
+++ phone.php
@@ -0,0 +1,46 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Phone</title>
+    <link rel="stylesheet" href="style4.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+       <img class="figure" src="logo_greta.jpg" alt="greta">
+       <div class="Profils"> <div class="information_generale"><div class="line11"></div><p id="profil2">profil</p><div class="text"><p id="nom">Jackson Arlene</p><p id="metier">President of Sales</p></p id="adresse">3 rue Jacques, Paris</p></div><div class="photo"><p id="changer">Changer la photo de profil</p><div class="photo1"></div>
+    </div>
+</div>
+       
+       <div class="contact1"><p id="contact2">contact</p><div class="telephone"><p id="telephone1">(+33)6 55 59 32 88</p><p id="telephone2">Téléphone</p></div><div class="mail1"><p id="email">Email</p><p id="email1">willie.jennings@example.com</p></div><div class="line12"></div>
+    </div>
+    <div class="inform_pro"><p id="inform_pro1">Information professionnelle</p><div class="text1"><p id="position">Position Actuelle</p><p id="metier1">President of sales</p><p id="google">Google</p><p id="adresse1">20 rue Jacques, Paris</p></div><div class="line13"></div><div class="photo2"><p id="modifier">Modifier le CV</p><div class="CV"></div></div></div>
+    <div class="Projet"><p id="projet1">Projet</p><div class="photo3"><img class="im7" src="image6.jpg" alt="greta1"><img class="im7" src="image7.jpg" alt="greta2"><img  class="im8" src="image8.jpg" alt="greta3"><img class="im9" src="image9.jpg" alt="greta4"></div></div>
+    </div>
+    </div>
+    <footer>
+    <div class="linear_bot"></div><div class="linear_top"></div>
+    <div class="nav-bar"><div class="compte"><div class="line6"></div><p id="compte1">Compte</p>
+</div>
+<div class="statistiques"><div class="line7"></div><p class="statistiques1">Statistiques de la formation</p>
+</div>
+<div class="Profil"><div class="line8"></div><p id="Profil1">Profil du stagiaire</p>
+</div>
+<div class="recherche"><div class="line10"></div><p id="recherche1">Recherche stage professionnel</p></div>
+</div>
+    <div class="contact"><div class="mail-tel"></div><div class="reseaux_sociaux"><p id="reseaux_sociaux1">Réseaux sociaux</p><div class="rectangle3"></div>
+</div>
+<div class="line4"></div>
+<p id="nous_contacter">Nous contacter</p>
+</div>
+    <div class="copy"><p id="greta1">Greta de l"essonne @ 2018</p></div>
+    
+</footer> 
+    </div>
+</body>
+</html>
