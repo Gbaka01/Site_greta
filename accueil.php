855b97bc74025937b26d807f47103a54b340f8fb
Gbaka01 <78058739+Gbaka01@users.noreply.github.com>
2022-05-19 18:12:13 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,68 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site greta</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css"/>
+ </head>
+<body >
+<div class="container">
+        <img class="figure" src="logo_greta.jpg" alt="greta"/> 
+
+   <div class="bienvenue"><img class="figure2" src="triangles-couleurs.png" alt="greta1"/>
+   <p id="site">Bienvenue sur le site du GRETA </p>
+   <p id="site_1"> Une plateforme de discussion entre les stagiaires de la formation et les professionnels</p> 
+   </div>
+
+   
+   <div class="stagiaire"> 
+    <div class="plus-d-info"> 
+        <input class="styled"
+       type="button"
+       value="En savoir plus">
+       <p id="info1">Voici toutes les informations qui pourraient être utile</p>
+          </div>
+        </div>
+        
+    
+            <div class="employeur">
+            <div class="plus-d-info1"><input class="styled2"
+       type="button"
+       value="En savoir plus">
+            <p id="info2">Apprenez en plus
+sur les différents
+corps de métiers de
+formation</p>
+</div>
+<div class="call-to-action">
+    <div class="plus-d-info2"><input class="styled3"
+       type="button"
+       value="En savoir plus"><p id="info3">Découvrez en plus sur les débouchés de nos formations</p></div>
+       <p id="info4">Besoin d'information supplémentaire?</p>
+</div>
+<footer>
+    <div class="linear_bot"></div><div class="linear_top"></div>
+    <div class="nav-bar"><div class="compte"><div class="line6"></div><p id="compte1">Compte</p>
+</div>
+<div class="statistiques"><div class="line7"></div><p id="statistiques1">Statistiques de la formation</p>
+</div>
+<div class="Profil"><div class="line8"></div><p id="Profil1">Profil du stagiaire</p>
+</div>
+<div class="recherche"><div class="line10"></div><p id="recherche1">Recherche stage professionnel</p></div>
+</div>
+    <div class="contact"><div class="mail-tel"></div><div class="reseaux_sociaux"><p id="reseaux_sociaux1">Réseaux sociaux</p><div class="rectangle8"></div>
+</div>
+<div class="line4"></div>
+<p id="nous_contacter">Nous contacter</p>
+</div>
+    <div class="copy"><p id="greta1">Greta de l'essonne @ 2018</p></div>
+    
+</footer>
+</div>
+</body>
+</html>
